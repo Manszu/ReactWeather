b016f9ec5a8974782847e93c22bd9b7158997ed2
Krzysztof Mansz <krzysztof.mansz@gmail.com>
2017-02-27 18:36:15 +0100

    comit calego projektu

--- example-promise.js
+++ example-promise.js
@@ -1,1 +1,12 @@
-function getTempCallback
+function getTempCallback (location, callback){
+    callback(undefined, 78); //case for no error
+    callback('City cannot be showed'); //case for error
+}
+
+getTempCallback('Philadelphia', function(err,temp) { //this is the start of the second parameter
+    if(err){
+        console.log('error', err);
+    } else{
+        console.log('success', temp);
+    }
+}); //this is the end of the second parameter
